4fa5608842cf1e7153828d1a14d668a0e8543fc5
Bombin_Valentin <wwall@yandex.ru>
2022-04-07 16:04:49 +0300

    Закончили скрипты. Начали работать

--- script/work.bat
+++ script/work.bat
@@ -3,8 +3,7 @@ set PROGRAMM="C:\Program Files\1cv8\8.3.20.1549\bin\1cv8.exe"
 set CURDIR=%~dp0..\
 set BINDIR=%CURDIR%bin
 
-call make_bd.bat
-call restoreIB.bat
+call compile.bat
 echo Run designer
 
 
