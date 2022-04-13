2c855d1b887faff06d2f4322cca5189f93439b4d
Bombin_Valentin <wwall@yandex.ru>
2022-04-13 18:34:26 +0300

    Ванесса интерактивно

--- script/compile.bat
+++ script/compile.bat
@@ -4,10 +4,11 @@ set PROGRAMM="C:\Program Files\1cv8\8.3.20.1549\bin\1cv8.exe"
 set CURDIR=%~dp0..\
 set BINDIR=%CURDIR%bin
 set SRCDIR=%CURDIR%src\cf
+set USER=dev
 
 call restoreIB.bat
 echo Load src into 1C base
 
-%PROGRAMM% DESIGNER /F %BINDIR% /LoadConfigFromFiles  %SRCDIR% 
+%PROGRAMM% DESIGNER /F %BINDIR% /N%USER% /LoadConfigFromFiles  %SRCDIR% 
 
 
--- script/decompile.bat
+++ script/decompile.bat
@@ -4,9 +4,9 @@ set PROGRAMM="C:\Program Files\1cv8\8.3.20.1549\bin\1cv8.exe"
 set CURDIR=%~dp0..\
 set BINDIR=%CURDIR%bin
 set SRCDIR=%CURDIR%src\cf
-
+set USER=dev
 echo Extract src from 1C base
 
-%PROGRAMM% DESIGNER /F %BINDIR% /DumpConfigToFiles  %SRCDIR% 
+%PROGRAMM% DESIGNER /F %BINDIR% /N%USER% /DumpConfigToFiles  %SRCDIR% 
 
 
